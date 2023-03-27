6e95bff9611bc8020ffc6bdd18162dd9ec88dd57
souravadhikar1 <souravadhikari09296@gmail.com>
2023-03-27 21:48:07 +0600

    cart set in local storage added

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { addToDb, getShoppingCart } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
 
@@ -6,6 +7,7 @@ import "./Shop.css";
 const Shop = () => {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState([]);
+
   useEffect(() => {
     fetch("products.json")
       .then((res) => res.json())
@@ -14,8 +16,11 @@ const Shop = () => {
   const handleAddToCart = (product) => {
     const newCart = [...cart, product];
     setCart(newCart);
+    addToDb(product.id);
   };
-
+  useEffect(() => {
+    const storedCart = getShoppingCart();
+  }, []);
   return (
     <div className="shop-container">
       <div className="product-container">
