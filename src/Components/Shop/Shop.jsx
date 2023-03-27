9a0a046c9fb3516a27f7e221ec2d09d366607aaf
souravadhikar1 <souravadhikari09296@gmail.com>
2023-03-27 22:22:14 +0600

    set saved cart added

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -13,14 +13,32 @@ const Shop = () => {
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+
+  useEffect(() => {
+    const storedCart = getShoppingCart();
+    const savedCart = [];
+    // step-1 get id
+    for (const id in storedCart) {
+      // get product from products state by using id
+      const addedProduct = products.find((product) => product.id === id);
+      if (addedProduct) {
+        // step-3 : add quantity
+        const quantity = storedCart[id];
+        addedProduct.quantity = quantity;
+        // step-4---- add the added product to the saved  cart
+        savedCart.push(addedProduct);
+      }
+    }
+    // step 5 : saved the cart
+    setCart(savedCart);
+  }, [products]);
+
   const handleAddToCart = (product) => {
     const newCart = [...cart, product];
     setCart(newCart);
     addToDb(product.id);
   };
-  useEffect(() => {
-    const storedCart = getShoppingCart();
-  }, []);
+
   return (
     <div className="shop-container">
       <div className="product-container">
